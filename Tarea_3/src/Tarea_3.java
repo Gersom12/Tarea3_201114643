16a807d1bc19fc7716217d33c6bdb501a6c577a0
Gersom12 <gersomeliezer@gmail.com>
2014-08-20 16:03:28 -0600

    Tarea 3 Proyecto en Java
    verificar si un numero es par o impar

---
+++ Tarea_3/src/Tarea_3.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class Tarea_3 {
+
+	
+	public static void main(String[] args) {
+		
+Scanner s=new Scanner(System.in);
+		int x;
+		System.out.println("Ingrese el numero");
+		x=s.nextInt();
+		
+		if (x%2!=0)
+		System.out.println("El número ingresado es impar");
+		else
+			System.out.println("El número ingresado es par");
+		
+		
+	}
+}
